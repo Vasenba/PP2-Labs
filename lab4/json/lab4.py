0853419ddee439bc1ab7eb881061516442e1b008
Yerassyl Baban <erasylbaban@gmail.com>
2024-02-19 23:56:03 +0600

    Uploaded

---
+++ lab4/json/lab4.py
@@ -0,0 +1,28 @@
+import json
+
+with open('data.json') as file:
+    json_data = json.load(file)
+
+
+
+
+
+print("Interface Status")
+print("================================================================================")
+print("DN                                                 Description           Speed    MTU")  
+print("-------------------------------------------------- --------------------  ------  ------")
+
+arr = json_data['imdata']
+
+for it in arr:
+    l1 = it["l1PhysIf"]
+    a = l1["attributes"]
+    dn = a['dn']
+    speed = a['speed']
+    mtu = a['mtu']
+    cout = ""
+    if(len(dn) == 42):
+        cout += dn + " " * 30 + speed + "   " + mtu
+    else:
+        cout += dn + " " * 31 + speed + "   " + mtu
+    print(cout)
